862325307fbbcecdb7fd33cea493b8717a0024cd
Ernie Turner <ernie.turner@ironcorelabs.com>
2020-01-30 16:04:36 -0700

    Remove unused methods

--- src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/BatchUnwrappedDocumentKeys.java
+++ src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/BatchUnwrappedDocumentKeys.java
@@ -5,8 +5,9 @@ import java.util.Map;
 import com.google.api.client.util.Key;
 
 /**
- * A map from an EDEK ID to a DEK decrypted with the tenants KMS, both in base64 format. Also
- * includes a map of failures if any problems occured when performing the batch unwrap operation.
+ * A map from an EDEK ID to a DEK decrypted with the tenants KMS, both in base64
+ * format. Also includes a map of failures if any problems occured when
+ * performing the batch unwrap operation.
  */
 public class BatchUnwrappedDocumentKeys {
     @Key
@@ -15,19 +16,11 @@ public class BatchUnwrappedDocumentKeys {
     @Key
     private Map<String, ErrorResponse> failures;
 
-    public Map<String, UnwrappedDocumentKey> getKeys(){
+    public Map<String, UnwrappedDocumentKey> getKeys() {
         return this.keys;
     }
 
-    public Map<String, ErrorResponse> getFailures(){
+    public Map<String, ErrorResponse> getFailures() {
         return this.failures;
-    }
-
-    public boolean hasSuccesses(){
-        return this.keys.size() > 0;
-    }
-
-    public boolean hasFailures(){
-        return this.failures.size() > 0;
     }
 }
--- src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/BatchWrappedDocumentKeys.java
+++ src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/BatchWrappedDocumentKeys.java
@@ -5,8 +5,9 @@ import java.util.Map;
 import com.google.api.client.util.Key;
 
 /**
- * A map from a document ID to a DEK and its encrypted form (EDEK) wrapped with the tenants KMS, both in base64 format. Also
- * includes a map of failures if any problems occured when performing the batch wrap operation.
+ * A map from a document ID to a DEK and its encrypted form (EDEK) wrapped with
+ * the tenants KMS, both in base64 format. Also includes a map of failures if
+ * any problems occured when performing the batch wrap operation.
  */
 public class BatchWrappedDocumentKeys {
     @Key
@@ -15,23 +16,11 @@ public class BatchWrappedDocumentKeys {
     @Key
     private Map<String, ErrorResponse> failures;
 
-    public Map<String, WrappedDocumentKey> getKeys(){
+    public Map<String, WrappedDocumentKey> getKeys() {
         return this.keys;
     }
 
-    public WrappedDocumentKey getKey(String index){
-        return this.keys.get(index);
-    }
-
-    public Map<String, ErrorResponse> getFailures(){
+    public Map<String, ErrorResponse> getFailures() {
         return this.failures;
-    }
-
-    public boolean hasSuccesses(){
-        return this.keys.size() > 0;
-    }
-
-    public boolean hasFailures(){
-        return this.failures.size() > 0;
     }
 }
