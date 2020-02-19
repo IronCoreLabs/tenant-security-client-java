0e6540960b5d2c2fc427f49a5d8ecec28c137cf4
Murph Murphy <murph@clurictec.com>
2020-02-19 11:36:51 -0700

    Add dropped file.

---
+++ src/test/java/com/ironcorelabs/tenantsecurity/kms/v1/TestSettings.java
@@ -0,0 +1,6 @@
+package com.ironcorelabs.tenantsecurity.kms.v1;
+
+public final class TestSettings {
+    public static String TSP_ADDRESS = "http://localhost";
+    public static String TSP_PORT = ":7777";
+}
