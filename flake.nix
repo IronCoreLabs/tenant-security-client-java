1540993b4aa8e4aaa94e3312b583bc4cc55143ca
Colt Frederickson <coltfred@gmail.com>
2023-06-15 11:20:38 -0600

    Add nix flake to tsc-java (#116)
    * Add nix shell.
    
    * nix flake update

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Tenant Security Client Java";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            maven
+            openjdk11
+          ];
+        };
+      }
+    );
+}
