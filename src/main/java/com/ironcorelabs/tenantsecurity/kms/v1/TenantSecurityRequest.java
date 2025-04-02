430f6b93d0e5d82957ff539d50e4f6e936ceef7e
Jason Yost <55884945+jason-yost-jamf@users.noreply.github.com>
2025-04-02 12:22:44 -0500

    Update pom.xml (#142)
    * Update pom.xml
    
    Added grpc-api to the include set for shading to ensure that grpc-context does not get pruned from the fat JAR.
    
    * Update pom.xml
    
    Updated to 7.2.2
    
    * Update the version sent to TSPs
    
    ---------
    
    Co-authored-by: Murph Murphy <murph@clurictec.com>

--- src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/TenantSecurityRequest.java
+++ src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/TenantSecurityRequest.java
@@ -55,7 +55,7 @@ final class TenantSecurityRequest implements Closeable {
   private final int timeout;
 
   // TSC version that will be sent to the TSP.
-  static final String sdkVersion = "7.2.1";
+  static final String sdkVersion = "7.2.2";
 
   TenantSecurityRequest(String tspDomain, String apiKey, int requestThreadSize, int timeout) {
     HttpHeaders headers = new HttpHeaders();
