acd6f160c6a8b73a4dd17282d9825121be5115e0
Colt Frederickson <coltfred@gmail.com>
2026-05-19 09:37:02 -0600

    Update version and changelog

--- src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/TenantSecurityRequest.java
+++ src/main/java/com/ironcorelabs/tenantsecurity/kms/v1/TenantSecurityRequest.java
@@ -61,7 +61,7 @@ final class TenantSecurityRequest implements Closeable {
   private final int connectTimeout;
 
   // TSC version that will be sent to the TSP.
-  static final String sdkVersion = "8.1.0-SNAPSHOT";
+  static final String sdkVersion = "8.1.0";
 
   TenantSecurityRequest(String tspDomain, String apiKey, int requestThreadSize, int timeout) {
     this(tspDomain, apiKey, requestThreadSize, timeout, timeout);
